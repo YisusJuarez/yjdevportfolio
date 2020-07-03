04b1115d9f0b0c650909f1cd1f3d84bf3ac862b6
Jesus Juarez <jesusjuarez@MacBook-Air-de-Jesus.local>
2020-07-03 17:55:29 -0500

    Robots

--- components/SkillsExp/SkillsExp.js
+++ components/SkillsExp/SkillsExp.js
@@ -9,22 +9,22 @@ class SkillsExp extends React.Component {
                 <Container maxWidth="lg">
                     <h1 className="titles-bg-black">Skills & Experience</h1>
                     <Grid container spacing={2}>
-                        <Grid item lg={4} md={6} sm={6}>
+                        <Grid item lg={4} md={6} sm={6} xs={6}>
                             <CardSkill Skill="Javascript" Descriptionskill="Programming Language commonly used in web development."></CardSkill>
                         </Grid>
-                        <Grid item lg={4} md={6} sm={6}>
+                        <Grid item lg={4} md={6} sm={6} xs={6}>
                             <CardSkill Skill="HTML5" Descriptionskill="HyperText Markup Language for websites"></CardSkill>
                         </Grid>
-                        <Grid item lg={4} md={6} sm={6}>
+                        <Grid item lg={4} md={6} sm={6} xs={6}>
                             <CardSkill Skill="CSS3" Descriptionskill="Cascading Style Sheets for Styling websites"></CardSkill>
                         </Grid>
-                        <Grid item lg={4} md={6} sm={6}>
+                        <Grid item lg={4} md={6} sm={6} xs={6}>
                             <CardSkill Skill="React" Descriptionskill="A JavaScript library for building user interfaces"></CardSkill>
                         </Grid>
-                        <Grid item lg={4} md={6} sm={6}>
+                        <Grid item lg={4} md={6} sm={6} xs={6}>
                             <CardSkill Skill="NextJs" Descriptionskill="React Server Side Rendering Applications (SSR)"></CardSkill>
                         </Grid>
-                        <Grid item lg={4} md={6} sm={6}>
+                        <Grid item lg={4} md={6} sm={6} xs={6}>
                             <CardSkill Skill="SEO" Descriptionskill="Increasing the visibility of a website to users of a web search engine."></CardSkill>
                         </Grid>
 
