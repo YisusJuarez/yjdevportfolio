103984d9eed3b992bb094c22fb0bbfd6006001c9
Jesus Juarez <jesusjuarez@MacBook-Air-de-Jesus.local>
2020-07-03 17:49:27 -0500

    Portafolio button

--- components/SkillsExp/SkillsExp.js
+++ components/SkillsExp/SkillsExp.js
@@ -9,18 +9,25 @@ class SkillsExp extends React.Component {
                 <Container maxWidth="lg">
                     <h1 className="titles-bg-black">Skills & Experience</h1>
                     <Grid container spacing={2}>
-                        <Grid item lg={4}>
-                            <CardSkill Skill="React" Descriptionskill="A JavaScript library for building user interfaces"></CardSkill>
+                        <Grid item lg={4} md={6} sm={6}>
+                            <CardSkill Skill="Javascript" Descriptionskill="Programming Language commonly used in web development."></CardSkill>
                         </Grid>
-                        <Grid item lg={4}>
-                            <CardSkill Skill="NextJs" Descriptionskill="React Server Side Rendereing Applications"></CardSkill>
-                        </Grid>
-                        <Grid item lg={4}>
+                        <Grid item lg={4} md={6} sm={6}>
                             <CardSkill Skill="HTML5" Descriptionskill="HyperText Markup Language for websites"></CardSkill>
                         </Grid>
-                        <Grid item lg={4}>
+                        <Grid item lg={4} md={6} sm={6}>
                             <CardSkill Skill="CSS3" Descriptionskill="Cascading Style Sheets for Styling websites"></CardSkill>
                         </Grid>
+                        <Grid item lg={4} md={6} sm={6}>
+                            <CardSkill Skill="React" Descriptionskill="A JavaScript library for building user interfaces"></CardSkill>
+                        </Grid>
+                        <Grid item lg={4} md={6} sm={6}>
+                            <CardSkill Skill="NextJs" Descriptionskill="React Server Side Rendering Applications (SSR)"></CardSkill>
+                        </Grid>
+                        <Grid item lg={4} md={6} sm={6}>
+                            <CardSkill Skill="SEO" Descriptionskill="Increasing the visibility of a website to users of a web search engine."></CardSkill>
+                        </Grid>
+
                     </Grid>
                 </Container>
                 <style jsx global>{`
