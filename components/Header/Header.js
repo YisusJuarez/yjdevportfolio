24467741b84ca030cb23ee3f359b6d5ae53d6a8c
YisusJuarez <jesusjuarez@MacBook-Air-de-Jesus.local>
2020-08-19 17:25:05 -0500

    ajustes

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -4,12 +4,12 @@ export default function Home() {
   return (
   
       <Head>
-        <title>Dotcom - YJDev</title>
+        <title>Wecode - Professional Web Development</title>
         <link rel="icon" href="/favicon.ico" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700&display=swap" />
-        <meta name="description" content="Professional development of websites and web systems with the latest technologies"></meta>
-        <meta name="author" content="Dotcom - YJDev"></meta>
-        <meta name="keywords" content="HTML, CSS, JavaScript"></meta>
+        <meta name="description" content="Professional web development with the latest technologies"></meta>
+        <meta name="author" content="Wecode"></meta>
+        <meta name="keywords" content="HTML, CSS, JavaScript, web, development , systems"></meta>
         <meta name="theme-color" content="#fefbfb"></meta>
       </Head>
      
