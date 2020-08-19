07119447e79ae90039a39cb563d63b98ed88d68f
YisusJuarez <jesusjuarez@MacBook-Air-de-Jesus.local>
2020-08-19 17:33:27 -0500

    ajustes

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -8,7 +8,7 @@ export default function Home() {
         <title>Wecode - Professional Web Development</title>
         <link rel="icon" href="/favicon.ico" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700&display=swap" />
-        <meta name="description" content="Professional web development with the latest technologies"></meta>
+        <meta name="description" content="Professional Web Development with the latest technologies, boost your brand with the power of UX, UI and Responsive Design."></meta>
         <meta name="author" content="Wecode"></meta>
         <meta name="keywords" content="HTML, CSS, JavaScript, web, development , systems"></meta>
         <meta name="theme-color" content="#fefbfb"></meta>
