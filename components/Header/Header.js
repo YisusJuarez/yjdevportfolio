a38d715e82214489ced2537c959019f0e0c55c7b
YisusJuarez <jesusjuarez@MacBook-Air-de-Jesus.local>
2020-08-19 17:56:09 -0500

    ajustes

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -4,14 +4,14 @@ export default function Home() {
   return (
   
       <Head>
-         <html lang='ES'/>
+         <html lang='en'/>
         <title>Wecode - Professional Web Development</title>
         <link rel="icon" href="/favicon.ico" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700&display=swap" />
         <meta name="description" content="Professional Web Development with the latest technologies, boost your brand with the power of UX, UI and Responsive Design."></meta>
         <meta name="author" content="Wecode"></meta>
         <meta name="keywords" content="HTML, CSS, JavaScript, web, development , systems"></meta>
-        <meta name="theme-color" content="#fefbfb"></meta>
+        <meta name="theme-color" content="#999999"/>
       </Head>
      
   
